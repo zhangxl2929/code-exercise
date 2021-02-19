378cf884d89d28a836866cee80dd9f2d0cc419cf
zhangxl2929 <zhangxiaolu@caicloud.io>
2021-02-19 17:51:10 +0800

    feat:观察者模式简单实现

---
+++ .history/code/observer_20210219174313.js
@@ -0,0 +1,23 @@
+class Observer{
+    constructor(){
+        this.events = {};
+    }
+    subsrible(eventName,callback) {
+        if (this.events[eventName]) {
+            this.events[eventName].push(callback);
+
+        } else {
+            this.events[eventName]=[callback]
+        }
+    }
+
+    unsubsrible(eventName,callback) {
+        const allCbs = this.events[eventName];
+        this.events[eventName] = allCbs.filter(item=>item!==callback)
+    }
+
+    publish(eventName) {
+        const allCbs = this.events[eventName];
+        allCbs.foreach(item=>{item&&item()})
+    }
+}
